8240c2dd8132e212e8bff977d6dfe0c9f119b491
lapjardim <lapjardim@lapjardim>
2018-03-27 04:59:15 -0300

    Publish compiled version

---
+++ src/service-module/mutations.js
@@ -0,0 +1,247 @@
+import _merge from 'lodash.merge'
+import _cloneDeep from 'lodash.clonedeep'
+import serializeError from 'serialize-error'
+import isObject from 'lodash.isobject'
+
+export default function makeServiceMutations (servicePath) {
+  function addItem (state, item) {
+    const { idField } = state
+    let id = item[idField]
+
+    // Only add the id if it's not already in the `ids` list.
+    if (!state.ids.includes(id)) {
+      state.ids.push(id)
+    }
+
+    state.keyedById = {
+      ...state.keyedById,
+      [id]: item
+    }
+  }
+
+  function updateItem (state, item) {
+    const { idField } = state
+    let id = item[idField]
+    state.keyedById[id] = item
+  }
+
+  return {
+    addItem (state, item) {
+      addItem(state, item)
+    },
+    addItems (state, items) {
+      items.forEach(item => addItem(state, item))
+    },
+    updateItem (state, item) {
+      updateItem(state, item)
+    },
+    updateItems (state, items) {
+      if (!Array.isArray(items)) {
+        throw new Error('You must provide an array to the `removeItems` mutation.')
+      }
+      items.forEach(item => updateItem(state, item))
+    },
+
+    removeItem (state, item) {
+      const { idField } = state
+      const idToBeRemoved = isObject(item) ? item[idField] : item
+      const keyedById = {}
+      const { currentId } = state
+
+      state.ids = state.ids.filter(id => {
+        if (id === idToBeRemoved) {
+          return false
+        } else {
+          keyedById[id] = state.keyedById[id]
+          return true
+        }
+      })
+
+      state.keyedById = keyedById
+
+      if (currentId === idToBeRemoved) {
+        state.currentId = null
+        state.copy = null
+      }
+    },
+
+    removeItems (state, items) {
+      const { idField } = state
+
+      if (!Array.isArray(items)) {
+        throw new Error('You must provide an array to the `removeItems` mutation.')
+      }
+      const containsObjects = items[0] && isObject(items[0])
+      const keyedById = {}
+      const currentId = state.currentId
+      let idsToRemove = items
+      const mapOfIdsToRemove = {}
+
+      // If the array contains objects, create an array of ids. Assume all are the same.
+      if (containsObjects) {
+        idsToRemove = items.map(item => item[idField])
+      }
+
+      // Make a hash map of the idsToRemove, so we don't have to iterate inside a loop
+      idsToRemove.forEach(idToRemove => {
+        mapOfIdsToRemove[idToRemove] = idToRemove
+      })
+
+      // Filter the ids to be those we're keeping. Also create new keyedById.
+      state.ids = state.ids.filter(id => {
+        if (mapOfIdsToRemove[id]) {
+          return false
+        } else {
+          keyedById[id] = state.keyedById[id]
+          return true
+        }
+      })
+
+      state.keyedById = keyedById
+
+      if (currentId && mapOfIdsToRemove[currentId]) {
+        state.currentId = null
+        state.copy = null
+      }
+    },
+
+    clearAll (state) {
+      state.ids = []
+      state.currentId = null
+      state.copy = null
+      state.keyedById = {}
+    },
+
+    clearList (state) {
+      let currentId = state.currentId
+      let current = state.keyedById[currentId]
+
+      if (currentId && current) {
+        state.keyedById = {
+          [currentId]: current
+        }
+        state.ids = [currentId]
+      } else {
+        state.keyedById = {}
+        state.ids = []
+      }
+    },
+
+    setCurrent (state, itemOrId) {
+      const { idField } = state
+      let id
+      let item
+      if (isObject(itemOrId)) {
+        id = itemOrId[idField]
+        item = itemOrId
+      } else {
+        id = itemOrId
+        item = state.keyedById[id]
+      }
+      state.currentId = id
+      state.copy = _cloneDeep(item)
+    },
+
+    clearCurrent (state) {
+      state.currentId = null
+      state.copy = null
+    },
+
+    // Deep assigns current to copy
+    rejectCopy (state) {
+      let current = state.keyedById[state.currentId]
+      _merge(state.copy, current)
+    },
+
+    // Deep assigns copy to current
+    commitCopy (state) {
+      let current = state.keyedById[state.currentId]
+      _merge(current, state.copy)
+    },
+
+    // Stores pagination data on state.pagination based on the query identifier (qid)
+    // The qid must be manually assigned to `params.qid`
+    updatePaginationForQuery (state, { qid, response, query }) {
+      const { data, limit, skip, total } = response
+      const { idField } = state
+      const ids = data.map(item => {
+        return item[idField]
+      })
+      state.pagination = { ...state.pagination, [qid]: { limit, skip, total, ids, query } }
+    },
+
+    setFindPending (state) {
+      state.isFindPending = true
+    },
+    unsetFindPending (state) {
+      state.isFindPending = false
+    },
+    setGetPending (state) {
+      state.isGetPending = true
+    },
+    unsetGetPending (state) {
+      state.isGetPending = false
+    },
+    setCreatePending (state) {
+      state.isCreatePending = true
+    },
+    unsetCreatePending (state) {
+      state.isCreatePending = false
+    },
+    setUpdatePending (state) {
+      state.isUpdatePending = true
+    },
+    unsetUpdatePending (state) {
+      state.isUpdatePending = false
+    },
+    setPatchPending (state) {
+      state.isPatchPending = true
+    },
+    unsetPatchPending (state) {
+      state.isPatchPending = false
+    },
+    setRemovePending (state) {
+      state.isRemovePending = true
+    },
+    unsetRemovePending (state) {
+      state.isRemovePending = false
+    },
+
+    setFindError (state, payload) {
+      state.errorOnFind = Object.assign({}, serializeError(payload))
+    },
+    clearFindError (state) {
+      state.errorOnFind = null
+    },
+    setGetError (state, payload) {
+      state.errorOnGet = Object.assign({}, serializeError(payload))
+    },
+    clearGetError (state) {
+      state.errorOnGet = null
+    },
+    setCreateError (state, payload) {
+      state.errorOnCreate = Object.assign({}, serializeError(payload))
+    },
+    clearCreateError (state) {
+      state.errorOnCreate = null
+    },
+    setUpdateError (state, payload) {
+      state.errorOnUpdate = Object.assign({}, serializeError(payload))
+    },
+    clearUpdateError (state) {
+      state.errorOnUpdate = null
+    },
+    setPatchError (state, payload) {
+      state.errorOnPatch = Object.assign({}, serializeError(payload))
+    },
+    clearPatchError (state) {
+      state.errorOnPatch = null
+    },
+    setRemoveError (state, payload) {
+      state.errorOnRemove = Object.assign({}, serializeError(payload))
+    },
+    clearRemoveError (state) {
+      state.errorOnRemove = null
+    }
+  }
+}
---
+++ src/service-module/service-module.js
@@ -0,0 +1,72 @@
+import { getShortName, getNameFromPath } from '../utils'
+import makeState from './state'
+import makeGetters from './getters'
+import makeMutations from './mutations'
+import makeActions from './actions'
+
+const defaults = {
+  idField: 'id', // The field in each record that will contain the id
+  autoRemove: false, // automatically remove records missing from responses (only use with feathers-rest)
+  nameStyle: 'short', // Determines the source of the module name. 'short', 'path', or 'explicit'
+  enableEvents: true, // Listens to socket.io events when available
+  state: {},     // for custom state
+  getters: {},   // for custom getters
+  mutations: {}, // for custom mutations
+  actions: {}    // for custom actions
+}
+
+export default function servicePluginInit (feathersClient, globalOptions = {}) {
+  if (!feathersClient || !feathersClient.service) {
+    throw new Error('You must provide a Feathers Client instance to feathers-vuex')
+  }
+
+  globalOptions = Object.assign({}, defaults, globalOptions)
+
+  return function createServiceModule (servicePath, options = {}) {
+    if (!feathersClient || !feathersClient.service) {
+      throw new Error('You must provide a service path or object to create a feathers-vuex service module')
+    }
+
+    options = Object.assign({}, globalOptions, options)
+    const { idField, autoRemove, nameStyle } = options
+
+    if (typeof servicePath !== 'string') {
+      throw new Error('The first argument to setup a feathers-vuex service must be a string')
+    }
+
+    const service = feathersClient.service(servicePath)
+    if (!service) {
+      throw new Error('No service was found. Please configure a transport plugin on the Feathers Client. Make sure you use the client version of the transport, like `feathers-socketio/client` or `feathers-rest/client`.')
+    }
+    const paginate = service.hasOwnProperty('paginate') && service.paginate.hasOwnProperty('default')
+
+    const defaultState = makeState(servicePath, { idField, autoRemove, paginate })
+    const defaultGetters = makeGetters(servicePath)
+    const defaultMutations = makeMutations(servicePath)
+    const defaultActions = makeActions(service)
+
+    return function setupStore (store) {
+      const nameStyles = {
+        short: getShortName,
+        path: getNameFromPath
+      }
+      let namespace = options.namespace || nameStyles[nameStyle](servicePath)
+
+      store.registerModule(namespace, {
+        namespaced: true,
+        state: Object.assign({}, defaultState, options.state),
+        getters: Object.assign({}, defaultGetters, options.getters),
+        mutations: Object.assign({}, defaultMutations, options.mutations),
+        actions: Object.assign({}, defaultActions, options.actions)
+      })
+
+      if (options.enableEvents) {
+        // Listen to socket events when available.
+        service.on('created', item => store.commit(`${namespace}/addItem`, item))
+        service.on('updated', item => store.commit(`${namespace}/updateItem`, item))
+        service.on('patched', item => store.commit(`${namespace}/updateItem`, item))
+        service.on('removed', item => store.commit(`${namespace}/removeItem`, item))
+      }
+    }
+  }
+}
